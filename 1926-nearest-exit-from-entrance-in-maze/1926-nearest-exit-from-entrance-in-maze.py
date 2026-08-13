86fc311b4e29466a5fa6f665183ec939f8b5a0cb
Deepansh Umar <umardeepansh@gmail.com>
2026-08-13 10:44:16 +0530

    Time: 89 ms (25.34%), Space: 21.4 MB (31.08%) - LeetHub

--- 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.py
+++ 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.py
@@ -1,12 +1,13 @@
+from collections import deque
 class Solution:
     def nearestExit(self, maze: List[List[str]], entrance: List[int]) -> int:
         n =len(maze)
         m = len(maze[0])
         visited = set()
-        queue = [tuple(entrance+[0])]
+        queue = deque([tuple(entrance+[0])])
         soln = []
         while(queue):
-            cell = queue.pop(0)
+            cell = queue.popleft()
             i,j  = cell[0],cell[1]
             if((i,j) in visited):
                 continue
@@ -14,11 +15,10 @@ class Solution:
             combo = [(i,j+1),(i,j-1),(i-1,j),(i+1,j)]
             steps = cell[2]
             if((i-1 == -1 or i+1 ==n or j-1 == -1 or j+1 ==m) and steps!=0):
-                soln.append(steps)
+                return steps
             for i,j in combo:
                 if(i>=0 and i<n and j>=0 and j<m):
                     if (maze[i][j] == "." and ((i,j) not in visited)):
                         queue.append((i,j,steps+1))
-        if soln:
-            return min(soln)
+        
         return -1
