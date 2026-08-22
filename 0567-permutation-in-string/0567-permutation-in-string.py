1e6fa2994a3ffaa7b9530fc1b12043c451d560a1
Deepansh Umar <umardeepansh@gmail.com>
2026-08-22 19:31:12 +0530

    Time: 103 ms (19.56%), Space: 19.3 MB (86.54%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,11 +1,40 @@
+from collections import Counter, defaultdict
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        m = len(s1)
+        def is_zero(d):
+            for key in d:
+                if d[key]>0:
+                    return False
+            return True
+        og = Counter(s1)
+        c = Counter(s1)
         n = len(s2)
-        s1 = "".join(sorted(list(s1)))
-        for i in range(n-m+1):
-            ns = s2[i:i+m]
-            ns= "".join(sorted(list(ns)))
-            if ns==s1:
-                return True
+        m = len(s1)
+        if(n<m):
+            return False
+        i,j = 0,0
+        while(i<=j and j<n):
+            if(j-i < m):
+                k = s2[j]
+                if( k in c and c[s2[j]]>0):
+                    c[s2[j]]-=1
+                    j+=1
+                elif(k not in c):
+                    j+=1
+                    i=j
+                    c = copy.deepcopy(og)
+                else:
+                    
+                    c[s2[i]]+=1
+                    i+=1
+                    
+            elif(j-i == m):
+                if(is_zero(c)):
+                    return True
+                else:
+                    j+=1
+                    c[s2[i]]+=1
+                    i+=1
+        if(is_zero(c)):
+            return True
         return False
